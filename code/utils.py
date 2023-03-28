f2fb23d0af479217d9c89773736d1a74dda1c450
Lenca Cuturela <cuturela@princeton.edu>
2023-03-28 18:29:18 -0400

    still cheecking zeta calculations against Iris' - a different indexing of phi (t vs t+1)

--- code/utils.py
+++ code/utils.py
@@ -6,3 +6,54 @@ import math
 import pickle
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+
+def reshapeObs(y):
+    '''
+    reshaping observation y from having C columns with values among 0 and 1 to having 1 column with values from 0 to C-1
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    '''
+    
+    yNew = np.empty((y.shape[0],))
+    if(y.shape[1] > 1):
+        yNew = np.array(np.where(y==1)[1]).reshape(y.shape[0],)
+    return yNew
+
+def accuracy(x,y,z,s):
+    '''
+    Calculates and plots percentage accuracy (given X and Y) and percentage accuracy in state 0 (given Z)
+
+    X: N x D numpy array
+    Y: N x 1 numpy array
+    s: int
+        number of sessions
+    '''
+    n = x.shape[0]
+    perf = np.zeros((s,))
+    trials = int(n/s)
+    state0 = np.empty((s,))
+    ind = []
+    for sess in range(0,s):
+        state0[sess] = trials - z[sess*trials:(sess+1)*trials].sum()
+        for t in range(0,trials):
+            if (x[sess*trials+t,1]>0 and y[sess*trials+t,1]==1):
+                perf[sess]+=1
+            elif (x[sess*trials+t,1]<0 and y[sess*trials+t,0]==1):
+                perf[sess]+=1
+            else:
+                ind.append(sess*trials+t)
+
+    perf = perf / trials # normalize to number of trials per session
+    state0 = state0 / trials
+    plt.plot(range(0,s),state0*100,marker='o',color='darkgray',label='in state 0')
+    plt.plot(range(0,s),perf*100,marker='o',label="accurracy")
+    plt.ylabel("percentage %")
+    plt.xlabel("sesion")
+    plt.legend(fontsize='small')
+    plt.show()
+
+    return perf, ind

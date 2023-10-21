fbe71e99b4defe00c5034cc7019545b7f10dcd00
Lenca Cuturela <cuturela@princeton.edu>
2023-10-21 08:54:36 -0400

    fitting subjects Remaining cluster dglmhmm1 sigmas

--- code/ClusterScriptPerAnimal.py
+++ code/ClusterScriptPerAnimal.py
@@ -14,18 +14,24 @@ import os
 
 ibl_data_path = '../data_IBL'
 dfAll = pd.read_csv(ibl_data_path + '/Ibl_processed.csv')
-df = pd.DataFrame(columns=['lab','subject','K']) # in total z=0,143
+df = pd.DataFrame(columns=['lab','subject','K']) # in total z=
 z = 0
 labChosen = ['churchlandlab'] #['angelakilab','churchlandlab','wittenlab']
-#subjectsRemaining = ['IBL-T3','CSHL_001','CSHL_002','CSHL_005','CSHL_007','CSHL_014','CSHL_015','ibl_witten_04','ibl_witten_05','ibl_witten_06','ibl_witten_07','ibl_witten_12','ibl_witten_13','ibl_witten_15','ibl_witten_16','NYU-01','NYU-04','NYU-06']
-for lab in labChosen:
-    subjects = np.unique(dfAll[dfAll['lab'] == lab]['subject']).tolist()
-    for subject in subjects:
-        for K in [1,2,3,4]:
-            # df.loc[z, 'lab'] = lab
-            df.loc[z, 'subject'] = subject
-            df.loc[z, 'K'] = K
-            z += 1
+subjectsRemaining = ['ibl_witten_05', 'ibl_witten_06','ibl_witten_07','CSHL049','CSHL_001','CSHL_002','CSHL_003','CSHL_004','CSHL_006','CSHL_012','CSHL_014','CSHL_015']
+# for lab in labChosen:
+#     subjects = np.unique(dfAll[dfAll['lab'] == lab]['subject']).tolist()
+#     for subject in subjects:
+#         for K in [1,2,3,4]:
+#             # df.loc[z, 'lab'] = lab
+#             df.loc[z, 'subject'] = subject
+#             df.loc[z, 'K'] = K
+#             z += 1
+for subject in subjectsRemaining:
+    for K in [1,2,3,4]:
+        # df.loc[z, 'lab'] = lab
+        df.loc[z, 'subject'] = subject
+        df.loc[z, 'K'] = K
+        z += 1
 # read from cluster array in order to get parallelizations
 idx = int(os.environ["SLURM_ARRAY_TASK_ID"])
 # lab = df.loc[idx,'lab']
